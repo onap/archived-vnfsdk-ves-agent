d7d88b37b168d61bafbe2559a2e30f1f8292336d
YatianXU <yatian.xu@nokia-sbell.com>
2019-09-07 04:33:35 -0400

    Change the for loop initial declarations to avoid adding -std=c99 in GFLAGS. As strdup() provided by POSIX returing "address out of bould" in C99 mode, which may cause program crash.
    Issue-ID: VNFSDK-465
    
    Signed-off-by: YatianXU <yatian.xu@nokia-sbell.com>
    Change-Id: I57f55edd939d64c2e201a345fe83659048308d50

--- veslibrary/ves_clibrary/evel/evel-library/code/evel_library/evel_pnf_registration.c
+++ veslibrary/ves_clibrary/evel/evel-library/code/evel_library/evel_pnf_registration.c
@@ -226,8 +226,8 @@ void evel_json_encode_pnf_registration(EVEL_JSON_BUFFER * jbuf,
       evel_json_checkpoint(jbuf);
       if (evel_json_open_opt_named_object(jbuf, "additionalFields"))
       {
-
-        for(unsigned int idx = 0; idx < ht->size; idx++ )
+        unsigned int idx = 0;
+        for(; idx < ht->size; idx++ )
         {
           /*****************************************************************/
           /* Get the first entry of a particular Key and loop through the  */
