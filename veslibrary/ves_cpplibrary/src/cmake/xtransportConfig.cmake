c9abdc27d88c4a428dd948fab5e029db52073cad
Yatian XU <yatian.xu@nokia-sbell.com>
2019-09-10 13:53:04 +0800

    Contribute C++ implement of VES spec 7.0.1 to ONAP/vnfsdk: Part5: test and code coverage
    Issue-ID: VNFSDK-466
    
    Signed-off-by: Yatian XU <yatian.xu@nokia-sbell.com>
    Change-Id: I75137726c32aa1f0ab146275b0cd32149cca0c38

--- veslibrary/ves_cpplibrary/src/cmake/xtransportConfig.cmake
+++ veslibrary/ves_cpplibrary/src/cmake/xtransportConfig.cmake
@@ -1,6 +1,8 @@
-find_library(XTRANSPORT_LIBRARY xtransport)
-find_path(XTRANSPORT_INCLUDE_DIR xvesagent/xtransport/XTransport.h)
-set(XTRANSPORT_INCLUDE_DIR "${XTRANSPORT_INCLUDE_DIR}/xvesagent/xtransport")
+find_library(XTRANSPORT_LIBRARY xtransport ${PROJECT_BINARY_DIR}/lib/transport)
+IF(XTRANSPORT_LIBRARY MATCHES "XTRANSPORT_LIBRARY-NOTFOUND")
+	set(XTRANSPORT_LIBRARY ${PROJECT_BINARY_DIR}/lib/transport/libxtransport.so)
+ENDIF()
+find_path(XTRANSPORT_INCLUDE_DIR XTransport.h ${CMAKE_SOURCE_DIR}/lib/transport/include)
 message("xtransport include dir: ${XTRANSPORT_INCLUDE_DIR}")
 message("xtransport library dir: ${XTRANSPORT_LIBRARY}")
 
---
+++ veslibrary/ves_cpplibrary/src/test/TransportSamples.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "XTransport.h"
+#include "EventSamples.h"
+
+void testLibcurlTransport();
+void testRetryTransport();
+void testSwitchableTransport();
+void testMemBufferedTransport();
+void testDiskBufferedTransport();
+void testRpcClientTransport();
+void testRpcServerTransport();
